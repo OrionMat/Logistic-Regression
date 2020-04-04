ac74b0edc7042dab3785aab2ff4be3cd88ac503a
OrionMat <36226295+OrionMat@users.noreply.github.com>
2020-04-04 19:52:39 -0400

    checks on fail cases

--- hand writted digits/main_handDigits.m
+++ hand writted digits/main_handDigits.m
@@ -30,16 +30,38 @@ probsTrain = sigmoid(X*allTheta');
 [_, p] = max(probsTrain, [], 2);
 fprintf('Fitted training accuracy:  %f%%\n', mean(double(p == y)) * 100);
 
-% randomly select 100 data points to display
-rand_indices = randperm(m);
-sel = X(rand_indices(1:100), 2:end);
-displayDigits(sel);
-
 % test accuracy
 mTest = length(ytest);
 Xtest = [ones(mTest, 1) Xtest]; % add bias
 probsTest = sigmoid(Xtest*allTheta');
 [_, pTest] = max(probsTest, [], 2);
 fprintf('Fitted test accuracy: %f%%\n', mean(double(pTest == ytest)) * 100);
+
+
+##% randomly select 100 data points to display
+##rand_indices = randperm(m);
+##sel = X(rand_indices(1:100), 2:end);
+##displayDigits(sel);
+
+##failIdxs = find(pTest != ytest)
+##displayDigits(Xtest(failIdxs(1:100), 2:end));
+
+%  randomly go throgh example cases displaying the image and the prediction
+rp = randperm(m);
+
+for i = 1:m
+  displayDigits(X(rp(i), 2:end));
+
+  probsTrain = sigmoid(X(rp(i), :)*allTheta');
+  [_, p] = max(probsTrain, [], 2);
+  
+  fprintf('Prediction: %d (digit %d)\n', p, mod(p, 10));
+  
+  % Pause with quit option
+  s = input('paused - press enter to continue, q to exit:','s');
+  if s == 'q'
+    break
+  end
+end
 
 
